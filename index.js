81226fae3724eaf42d249f81e4f57da83f75ec88
pankaj betewad <pankajbetewad@gmail.com>
2025-08-16 10:20:31 +0000

    added steps

--- index.js
+++ index.js
@@ -13,4 +13,29 @@ function printStar(n){
         console.log(row);
     }
 }
-printStar(5);
+// printStar(5);
+
+// *
+// * *
+// * * *
+// * * * *
+function steps(n){
+    for(let i = 0; i < n; i++){
+        let step = ""
+        for(let j = 0; j <= i; j++){
+            step = step + "* "
+        }
+        console.log(step)
+    }
+    // for(let i = 0; i < n; i++){
+    //     let step = ""
+    //     for(let j = 0; j < n; j++){
+    //         if(j <= i){
+    //             step = step + "* "
+    //         }
+    //     }
+    //     console.log(step)
+    // }
+}
+
+steps(3)
