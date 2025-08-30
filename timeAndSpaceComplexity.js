9335a2a19b960d8d3d5e22c2b7c1f9d19c2bcc33
pankajbetewad <pankajbetewad@gmail.com>
2025-08-30 18:06:02 +0530

    added time and space complexity details

--- timeAndSpaceComplexity.js
+++ timeAndSpaceComplexity.js
@@ -1,3 +1,22 @@
 /**
  * Time complexity is used to measure efficiency of algorithm in terms of speed, as the input size grows
+ * Linear search - O(n) vs binary search - O(log n)
+ * Big O notation - worst case time complexity
+ * o(log n) has better efficiency than o(n)
+ * O(n) - for - linear search
+ * O(log n) - binary search
+ * O(n**2) - nested for
+ * O(n logn) - merge sort
+ * O(2**n) - 
+ * O(n!) - 
+ * O(1) - constant time complexity
+ * 
+ * O(1) > O(logn) > O(n) > O(nlogn) > O(n**2) > O(2**n) > O(n!)
+ * 
+ * 
+ * 
+ * 
+ * ************************* space complexity **********************
+ * 
+ * 
  */
